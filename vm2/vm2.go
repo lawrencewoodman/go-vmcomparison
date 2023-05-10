a3160a302b43f8ddc63472fac3e7e884dfcacb4c
Lawrence Woodman <lwoodman@vlifesystems.com>
2023-05-10 11:19:14 +0100

    vm2, remove mask32 in JMP instruction

--- vm2/vm2.go
+++ vm2/vm2.go
@@ -125,7 +125,7 @@ func (v *VM2) execute(opcode uint, operandA uint, operandB uint) (bool, error) {
 			v.pc += 2
 		}
 	case 5 << 24: // JMP
-		v.pc = mask32(operandA + operandB)
+		v.pc = operandA + operandB
 	case 6 << 24: // LIT
 		//fmt.Printf("PC: %d  LIT  A: %d, B: %d\n", v.pc, operandA, operandB)
 		v.mem[operandB] = operandA
